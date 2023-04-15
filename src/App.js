9dfc5748f057ee409ab3fa3937039266940e4cde
Kat <kemetcalf3.14@gmail.com>
2023-04-14 18:03:25 -0700

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,88 +1,84 @@
-import './App.css';
-// import Item from './MyItem';
-import React from 'react';
+import "./App.css";
+import React from "react";
+import {
+	QueryClient,
+	QueryClientProvider,
+	useQuery,
+} from "@tanstack/react-query";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools/build/lib/devtools";
+import { useState } from "react";
+
+const queryClient = new QueryClient();
 
 class AffiliationRow extends React.Component {
-  render() {
-    return (
-      <li>
-       {this.props.organization}
-      </li>
-    )
-  }
+	render() {
+		return <li>{this.props.organization}</li>;
+	}
 }
 
-class StarWars extends React.Component{
-  constructor() {
-    super();
-    this.state = {
-      loadedCharacter: false,
-      image: null,
-      name: null,
-      height: null,
-      homeworld: null,
-      affiliations: [],
-    };
-  }
-  getNewCharacter() {
-    const randomNumber = Math.round(Math.random() * 88);
-    const url = `https://akabab.github.io/starwars-api/api/id/${randomNumber}.json`;
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        this.setState({
-          image: data.image,
-          name: data.name,
-          height: data.height,
-          homeworld: data.homeworld,
-          affiliations: data.affiliations,
-          loadedCharacter: true,
-        })
-      })
+function GetNewCharacter() {
+	const [characterId, setCharacterId] = useState(1);
+	function handleClick() {
+		setCharacterId(Math.round(Math.random() * 88));
+	}
+	const { status, error, data, isFetching } = useQuery({
+		queryKey: ["character", characterId],
+		queryFn: () =>
+			fetch(`https://akabab.github.io/starwars-api/api/id/${characterId}.json`)
+				.then((response) => response.json())
+				.then((data) => {
+					return {
+						image: data.image,
+						name: data.name,
+						height: data.height,
+						homeworld: data.homeworld,
+						affiliations: data.affiliations,
+						loadedCharacter: true,
+					};
+				}),
+	});
 
-  };
+	if (isFetching) return "Accessing Galactic Republic records...";
 
-  render() {
-    
-      const groups = this.state.affiliations.map((group, i) => {
-        return <AffiliationRow key={i} organization={group}/>
-      })
-    
-    return (
-      <div>
-        {
-          this.state.loadedCharacter && 
-          <div>
-            <img src={this.state.image} alt="character headshot"></img>
-            <h1>{this.state.name}</h1>
-            <p>{this.state.height} m</p>
-            <p>Homeworld: {this.state.homeworld}</p>
-            <ul>
-              {groups}
-            </ul>
-          </div>
-        }
+	if (status === "loading") return "Loading Galactic Republic records...";
 
-        <button 
-          type="button" 
-          onClick={() => this.getNewCharacter()}
-          className="btn"
-          >
-          Randomize Character
-        </button>
-      </div>
-    )
-  }
+	if (status === "error") return `An error has occurred: ${error.message}`;
+
+	console.info({ data, error, body: data.body });
+
+	const groups = data.affiliations.map((group, i) => {
+		return <AffiliationRow key={i} organization={group} />;
+	});
+
+	return (
+		<div>
+			<div>
+				<img src={data.image} alt="character headshot"></img>
+				<h1>{data.name}</h1>
+				<p>{data.height} m</p>
+				<p>Homeworld: {data.homeworld}</p>
+				<h2>Affiliations</h2>
+				<ul>{groups}</ul>
+			</div>
+
+			<button type="button" onClick={handleClick} className="btn">
+				Randomize Character
+			</button>
+		</div>
+	);
 }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <StarWars />
-      </header>
-    </div>
-  );
+	return (
+		<QueryClientProvider client={queryClient}>
+			<div className="App">
+				<header className="App-header">
+					<GetNewCharacter />
+				</header>
+			</div>
+			{/* <ReactQueryDevtools initialIsOpen /> */}
+		</QueryClientProvider>
+	);
 }
 
 export default App;
